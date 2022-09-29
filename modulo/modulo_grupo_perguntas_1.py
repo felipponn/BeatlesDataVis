bd4dcd63ce1333a566495e2c3a095fea483d9289
atronee <otavio.oam@protonmail.com>
2022-09-29 18:00:09 -0300

    Modifique as funções

--- modulo/modulo_grupo_perguntas_1.py
+++ modulo/modulo_grupo_perguntas_1.py
@@ -1,6 +1,54 @@
 import pandas as pd
 
-# dt = pd.read_csv("C:/Users/otavi/Desktop/FGV/Python/A1 LP/TheBeatlesCleaned.csv")
+def mais_x_por_album(dataframe, coluna):
+    try:
+        lista_albums = []
+        albums= pd.unique(dataframe["album"]) # lista com o nome dos albums 
+        for album in albums:
+            musicas = dataframe.loc[(dataframe["album"] == album)] #pega as linhas com músicas do album
+            musicas_ordenadas = musicas.sort_values(by=coluna, ascending=False)["song"] # ordena pela coluna
+            dt = pd.DataFrame(data=musicas_ordenadas) # transforma em DataFrame
+            dt.reset_index(inplace=True) # Tira o index do dataframe antigo
+            del dt['index']
+            lista_albums.append(dt)
+        return lista_albums
+
+    except TypeError:
+        print("te")
+    except KeyError:
+        print("ke")
+    except Exception as error:
+        return error
+
+
+def mais_x_da_banda(dataframe, coluna):
+    try:
+        dataframe.sort_values(by=coluna, ascending=False, inplace=True)
+        return dataframe[["song", coluna]]
+
+    except AttributeError:
+        print("ae")
+    except KeyError:
+        print("ke")
+    except Exception as error:
+        return error
+
+
+def mais_premiados_album(dataframe, coluna):
+    try:
+        dataframe= dataframe.loc[:, ('album', coluna)]
+        dataframe.drop_duplicates(inplace=True)
+        dataframe.sort_values(by=coluna, ascending=False, inplace=True)
+        dataframe.reset_index(inplace=True)
+        del dataframe['index']
+        return dataframe
+
+    except KeyError:
+        print("Ke")
+    except AttributeError:
+        print("Ae")
+    except Exception as error:
+        return error
 
 def mais_ouvidas_album(dataframe, coluna):
     lista_albums = []
@@ -11,23 +59,3 @@ def mais_ouvidas_album(dataframe, coluna):
         lista_albums.append(serie)
 
     return lista_albums
-
-
-def mais_longas_album(dataframe, coluna):
-    dataframe.sort_values(by=coluna)
-    
-
-def mais_ouvidas_banda(dataframe, coluna):
-    dataframe.sort_values(by=coluna, ascending=False, inplace=True)
-    return dataframe[coluna].head(10)
-
-def mais_longas_banda(dataframe, coluna):
-    dataframe.sort_values(by=coluna, ascending=False, inplace=True)
-    return dataframe[["song", coluna]].head(10)
-
-def mais_premiados_album(dataframe, coluna):
-    return
-
-
-# print(dt)
-# print(mais_longas_banda(dt, "duration_ms"))
