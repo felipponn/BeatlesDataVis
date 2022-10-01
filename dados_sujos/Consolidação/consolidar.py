2042c52549af2d107c4f3d21c91f584e9e24ab2c
felipponn <marques.luis@fgv.edu.br>
2022-09-30 22:54:31 -0300

    Crie a pasta de consolidação do dataset

---
+++ dados_sujos/Consolidação/consolidar.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+df1 = pd.read_csv("BeatlesDB.csv", sep='|')[:193]
+df2 = pd.read_csv("Letras.csv", sep="|", encoding="macintosh")
+
+lista_albuns = ["Please Please Me", "With the Beatles", "A Hard Day's Night", "Beatles for Sale", "Help!", "Rubber Soul", "Revolver", "Sgt. Pepper's Lonely Hearts Club Band", "Magical Mystery Tour", "The Beatles (white album)", "Yellow Submarine", "Abbey Road", "Let It Be"]
+
+df2_mask = df2["album"].isin(lista_albuns)
+df2 = df2[df2_mask]
+df2 = df2.rename(columns={"title":"song"})
+df2 = df2.drop(columns=["year"])
+
+beatles = df1.merge(df2, left_on=("song", "album"), right_on=("song", "album"), how="left").set_index(["album", "song"]).sort_index()
