bada74e95910e36480ed8fb9a8f0f26ffdfa807b
atronee <otavio.oam@protonmail.com>
2022-10-08 16:59:38 -0300

    Modifique o modulo seaborn_vis

--- seaborn_plots/seaborn_vis.py
+++ seaborn_plots/seaborn_vis.py
@@ -1,14 +1,15 @@
 from modulo import *
-from pacote_modulos import *
-import modulo_grupo_perguntas_1 as mp
+from modulo import modulo_grupo_perguntas_1 as mp
 import seaborn as sns
 import pandas as pd
 import matplotlib.pylab as plt
 
-beatlesDF = dataset.beatlesDF
-# beatlesDF = dataset.beatlesDF
-
 def musicas_mais_populares_da_banda(dataframe):
+    """Salva o Plot das musicas mais populares da banda
+
+    :param dataframe: Dataframe com musicas da banda
+    :type dataframe: pandas dataframe
+    """
     try:
         dataframe = dataframe.reset_index() # tira o multindex do dataframe
         plt.figure(figsize=(12, 6))         # cria a janela do gráfico
@@ -23,12 +24,18 @@ def musicas_mais_populares_da_banda(dataframe):
         plot.set_ylabel( "Popularidade", size = 12)   # eixo y
         plot.set_ylim(50, 90)                         # quando do eixo será mostrado
         fig = plot.get_figure()
-        fig.savefig("./img/Musicas Mais Populares dos Beatles.png")  # salva a figura
+        fig.savefig("img/Musicas Mais Populares dos Beatles.png")  # salva a figura
+        plt.close()
     except Exception as error:
         return error
 
 
 def musicas_menos_populares_da_banda(dataframe):
+    """Salva o Plot das musicas menos populares da banda
+
+    :param dataframe: Dataframe com musicas da banda
+    :type dataframe: pandas dataframe
+    """
     try:
         dataframe = dataframe.reset_index()  # tira o multindex do dataframe
         plt.figure(figsize=(12, 6))          # cria a janela do gráfico
@@ -43,12 +50,18 @@ def musicas_menos_populares_da_banda(dataframe):
         plot.set_ylabel( "Popularidade", size = 12)
         plot.set_ylim(0, )
         fig = plot.get_figure()
-        fig.savefig("./img/Musicas Menos Populares dos Beatles.png")
+        fig.savefig("img/Musicas Menos Populares dos Beatles.png")
+        plt.close()
     except Exception as error:
         return error
 
 
 def musicas_mais_longas_da_banda(dataframe):
+    """Salva o Plot das musicas mais longas da banda
+
+    :param dataframe: Dataframe com musicas da banda
+    :type dataframe: pandas dataframe
+    """
     try:
         dataframe = dataframe.reset_index()
         dataframe["duration_sec"]=dataframe["duration_ms"]/1000 # transforma ms em segundos
@@ -64,12 +77,18 @@ def musicas_mais_longas_da_banda(dataframe):
         plot.set_ylabel( "Duração (sec)", size = 12)
         plot.set_ylim(0, )
         fig = plot.get_figure()
-        fig.savefig("./img/Musicas Mais Longas dos Beatles.png")
+        fig.savefig("img/Musicas Mais Longas dos Beatles.png")
+        plt.close()
     except Exception as error:
         return error
 
 
 def musicas_mais_curtas_da_banda(dataframe):
+    """Salva o Plot das musicas mais curtas da banda
+
+    :param dataframe: Dataframe com musicas da banda
+    :type dataframe: pandas dataframe
+    """
     try:
         dataframe = dataframe.reset_index()
         dataframe["duration_sec"]=dataframe["duration_ms"]/1000
@@ -85,13 +104,21 @@ def musicas_mais_curtas_da_banda(dataframe):
         plot.set_ylabel( "Duração (sec)", size = 12)
         plot.set_ylim(0, 70)
         fig = plot.get_figure()
-        fig.savefig("./img/Musicas Mais Curtas dos Beatles.png")
+        fig.savefig("img/Musicas Mais Curtas dos Beatles.png")
+        plt.close()
     except Exception as error:
         return error
 
 
 def albums_mais_premiados_da_banda(dataframe):
+    """Salva o plot dos albuns mais premiados da banda
+
+    :param dataframe: Dataframe com musicas da banda
+    :type dataframe: pandas dataframe
+    """
     try:
+        dataframe["num_awards"] = [len(x.split(",")) if x != '[]' else 0 for x in dataframe["awards"]] # conta quantas premios tem um album
+        dataframe["num_nominations"] = [len(x.split(",")) if x != '[]' else 0 for x in dataframe["nominations"]] # conta quantas nomeações tem um album
         plt.figure(figsize=(12, 6))
         plot = sns.barplot(
         x="album",
@@ -104,12 +131,18 @@ def albums_mais_premiados_da_banda(dataframe):
         plot.set_ylabel( "Prêmios", size = 12)
         plot.set_ylim(0, )
         fig = plot.get_figure()
-        fig.savefig("./img/Albuns Mais Premiados dos Beatles.png")
+        fig.savefig("img/Albuns Mais Premiados dos Beatles.png")
+        plt.close()
     except Exception as error:
         return error
 
 
 def relacao_popularidade_duração(dataframe):
+    """Salva o plot da relação de popularidade e duração 
+
+    :param dataframe: Dataframe com musicas da banda
+    :type dataframe: pandas dataframe
+    """
     try:
         dataframe["duration_sec"]=dataframe["duration_ms"]/1000
         plt.figure(figsize=(8, 8))
@@ -125,12 +158,18 @@ def relacao_popularidade_duração(dataframe):
         plt.text(200, 2, f'Correlação:{corr}', fontsize = 10) # texto mostrando a correlação no plot
         plot.set_ylim(0, )
         fig = plot.get_figure()
-        fig.savefig("./img/Popularidade x Duração.png")
+        fig.savefig("img/Popularidade x Duração.png")
+        plt.close()
     except Exception as error:
         return error
 
 
 def mais_longas_por_album(data):
+    """Salva os plots das musicas mais longas por album
+
+    :param data: Dataframe com musicas da banda
+    :type data: pandas dataframe
+    """
     try:
         data["duration_sec"]=data["duration_ms"]/1000 # transforma ms em segundos
         albums= data.index.levels[0] # lista com o nome dos albuns em ordem alfabética
@@ -149,13 +188,19 @@ def mais_longas_por_album(data):
             plot.set_xlabel( "Músicas" , size = 12 )
             plot.set_ylabel( "Duração", size = 12)
             fig = plot.get_figure()
-            fig.savefig(f"./img/Mais Longas do {albums[contador]}.png")
-        contador+=1
+            fig.savefig(f"img/Mais Longas do {albums[contador]}.png")
+            plt.close()
+            contador+=1
     except Exception as error:
         return error
 
 
 def mais_curtas_por_album(data):
+    """Salva os plots das musicas mais curtas por album
+
+    :param data: Dataframe com musicas da banda
+    :type data: pandas dataframe
+    """
     try:
         data["duration_sec"]=data["duration_ms"]/1000 # transforma ms em segundos
         albums= data.index.levels[0]
@@ -174,13 +219,19 @@ def mais_curtas_por_album(data):
             plot.set_xlabel( "Músicas" , size = 12 )
             plot.set_ylabel( "Duração", size = 12)
             fig = plot.get_figure()
-            fig.savefig(f"./img/Mais curtas do {albums[contador]}.png")
+            fig.savefig(f"img/Mais curtas do {albums[contador]}.png")
+            plt.close()
             contador+=1
     except Exception as error:
         return error
 
 
 def mais_populares_por_album(data):
+    """Salva os plots das musicas mais populares por album
+
+    :param data: Dataframe com musicas da banda
+    :type data: pandas dataframe
+    """
     try:
         albums= data.index.levels[0]
         contador = 0
@@ -198,13 +249,19 @@ def mais_populares_por_album(data):
             plot.set_xlabel( "Músicas" , size = 12 )
             plot.set_ylabel( "Popularidade", size = 12)
             fig = plot.get_figure()
-            fig.savefig(f"./img/Mais Populares do {albums[contador]}.png")
+            fig.savefig(f"img/Mais Populares do {albums[contador]}.png")
+            plt.close()
             contador+=1
     except Exception as error:
         return error
 
 
 def menos_populares_por_album(data):
+    """Salva os plots das musicas menos populares por album
+
+    :param data: Dataframe com musicas da banda
+    :type data: pandas dataframe
+    """
     try:
         albums= data.index.levels[0]
         contador = 0
@@ -222,7 +279,105 @@ def menos_populares_por_album(data):
             plot.set_xlabel( "Músicas" , size = 12 )
             plot.set_ylabel( "Popularidade", size = 12)
             fig = plot.get_figure()
-            fig.savefig(f"C:/Users/otavi/Desktop/FGV/Python/A1 LP/img/Menos Populares do {albums[contador]}.png")
+            fig.savefig(f"img/Menos Populares do {albums[contador]}.png")
+            plt.close()
             contador+=1
+    except Exception as error:
+        return error
+
+
+def plot_participacao_vocal_album(dataframe):
+    """Salva o lineplot da participação vocal pelos albuns da banda
+
+    :param data: Dataframe com as músicas
+    :type data: pandas dataframe
+    """
+    try:
+        plt.figure(figsize=(12, 6))         # cria a janela do gráfico
+        plot = sns.lineplot(data=dataframe, # lineplot
+        y="participacao", 
+        x="album", 
+        hue="membro", 
+        hue_order=["Lennon", "McCartney", "Harrison", "Starr"]) # ordem das linhas
+        plot.set(title="Média de Participação vocal por Album")
+        plot.set_xlabel( "Album" , size = 12 )
+        plot.set_ylabel( "Média Vocal", size = 12)
+        plot.set_ylim(0, )
+        plt.xticks(rotation=90) # gira o nome dos albuns em 90 graus
+        plt.tight_layout() #deixa todos os textos na janela
+        fig = plot.get_figure()
+        fig.savefig("img/participação_vocal_album.png")
+        plt.close()
+    except Exception as error:
+        return error
+    
+
+def plotar_media_popularidade_vocal(data):
+    """Salva o plot da media popularidade pelos vocalistas da banda
+
+    :param data: Dataframe com as músicas
+    :type data: pandas dataframe
+    """
+    try:
+        plt.figure(figsize=(12, 6))         # cria a janela do gráfico
+        plot = sns.barplot(
+        data=data, 
+        y="Popularidade Média", 
+        x=data["Vocalista"].head(5),
+        color="#7BC5FF")
+        plot.set(title="Média de Popularidade por vocalista")
+        plot.set_xlabel( "Vocalista" , size = 12 )
+        plot.set_ylabel( "Média de Popularidade", size = 12)
+        plot.set_ylim(0, )
+        fig = plot.get_figure()
+        fig.savefig("img/popularidade_vocalista.png")
+        plt.close()
+    except Exception as error:
+        return error
+
+def plotar_media_popularidade_compositor(data):
+    """Salva o plot da media popularidade pelos compositores
+
+    :param data: Dataframe com as músicas
+    :type data: pandas dataframe
+    """
+    try:
+        plt.figure(figsize=(12, 6))         # cria a janela do gráfico
+        plot = sns.barplot(
+        data=data, 
+        y="Popularidade Média", 
+        x=data["Compositor"].head(5),
+        color="#7BC5FF")
+        plot.set(title="Média de Popularidade por compositor")
+        plot.set_xlabel( "Compositor" , size = 12 )
+        plot.set_ylabel( "Média de Popularidade", size = 12)
+        plot.set_ylim(0, )
+        fig = plot.get_figure()
+        fig.savefig("img/popularidade_compositor.png")
+        plt.close()
+    except Exception as error:
+        return error
+    
+def verborragia_album(dataframe):
+    """Salva o plot da verborragia media por album
+
+    :param data: Dataframe com as músicas
+    :type data: pandas dataframe
+    """
+    try:
+        plt.figure(figsize=(12, 6))
+        plot = sns.lineplot(
+        data=dataframe, 
+        y="Média de Palavras/seg", 
+        x="Álbum")
+        plot.set(title="Verborragia por Album")
+        plot.set_xlabel( "Album" , size = 12 )
+        plot.set_ylabel( "Palavras por segundo", size = 12)
+        plot.set_ylim(0, )
+        plt.xticks(rotation=90)
+        plt.tight_layout()
+        fig = plot.get_figure()
+        fig.savefig("img/verborragia_album.png")
+        plt.close()
     except Exception as error:
         return error
