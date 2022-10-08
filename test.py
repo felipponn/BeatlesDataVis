6fa152f0e1b05fb6b32eb3671532647085aa75e7
GabrielJP314 <gabrielpereirajp1201@gmail.com>
2022-10-08 15:46:46 -0300

    Exclua o arquivo test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from modulo import *
-from modulo.modulo_grupo_perguntas_2 import palavras_comuns_albuns, palavras_comuns_letras_albuns, palavras_comuns_letras_disco, palavras_comuns_musicas, titulo_album_letras, titulo_musica_letras
-from pacote_modulos import *
-
-df = dataset.beatlesDF
-
-print(titulo_musica_letras(df))
